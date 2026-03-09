92d26840cf720b4c962e7d9e2a232c62681a03a1
kimhongyeon <kimhongyeon89@gmail.com>
2026-03-09 20:58:01 +0900

    Update ESLint configuration for stricter TypeScript checks and unused imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,17 +4,23 @@ import tsEslint from 'typescript-eslint';
 
 export default tsEslint.config(
   eslint.configs.recommended,
-  tsEslint.configs.recommended,
+  tsEslint.configs.recommendedTypeChecked,
   {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
     plugins: {
       'unused-imports': unusedImports,
     },
     rules: {
+      '@typescript-eslint/no-deprecated': 'error',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-namespace': 'off',
-      "@typescript-eslint/consistent-type-imports": "error",
       'unused-imports/no-unused-imports': 'error',
       'unused-imports/no-unused-vars': [
         'warn',
@@ -27,6 +33,23 @@ export default tsEslint.config(
       ],
       '@typescript-eslint/ban-ts-comment': 'warn',
       'no-async-promise-executor': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/no-redundant-type-constituents': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/no-base-to-string': 'off',
+      '@typescript-eslint/prefer-promise-reject-errors': 'off',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+      '@typescript-eslint/await-thenable': 'off',
+      '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+      '@typescript-eslint/only-throw-error': 'off',
     },
   },
 );
